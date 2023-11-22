bfde5a25a51e3d1130a566c82058edf0aabe9614
Ayan-farhad <khaanayan81@gmail.com>
2023-11-23 00:43:26 +0500

    Calculator done

---
+++ app.js
@@ -0,0 +1,19 @@
+function calCue(click) {
+    let inp = document.getElementById("input").value += click;
+      //console.log(e);
+}
+function equal() {
+    let result = document.getElementById("input");
+    result.value = eval(result.value)
+}
+function deletAll() {
+    document.getElementById("input").value = '';
+}
+function delet() {
+    let a = document.getElementById("input");
+    let b = a.value;
+    if (b.length > 0) {
+        let res = b.slice(0, -1);
+        a.value = res;
+    }
+}
